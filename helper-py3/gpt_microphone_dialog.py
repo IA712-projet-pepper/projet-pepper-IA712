d9e89d8b20b7f1e5f0d55c1cfdb5fbb0ae53aab8
Joachim Carvallo <49419043+JoachimCarvallo@users.noreply.github.com>
2021-06-08 16:24:16 +0200

    Update gpt_microphone_dialog.py

--- helper-py3/gpt_microphone_dialog.py
+++ helper-py3/gpt_microphone_dialog.py
@@ -4,6 +4,7 @@ import torch
 import speech_recognition as sr
 import os
 import time
+import wikipedia
 
 # Load Dialogue GPT
 tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-large")
@@ -15,8 +16,8 @@ r = sr.Recognizer()
 print("Models loaded.")
 
 path = "C:/Users/joach/Downloads/Pepper/"
-count = 0
 chat_history_ids = torch.tensor([])
+wiki_search = False
 
 # Calibrating background noise
 print("- DialoGPT: Please wait. Calibrating microphone...")
@@ -41,26 +42,47 @@ while True:
             text = r.recognize_google(audio, language = 'en-US')
         except: # If speech recognizion doens't work
             print("- DialoGPT: I did not understand...")
-            text = "I did not understand"
+            response = "I did not understand. Can you repeat?"
             
             # Export the result 
-            with open('response.txt', mode ='w') as file:
+            with open('response.txt', mode ='w', encoding="utf-8") as file:
                 file.write(response)
 
             time.sleep(0.3)
             continue
         
-        # Generate an answer
         print("- User: {}".format(text))
-        new_user_input_ids = tokenizer.encode(text + tokenizer.eos_token, return_tensors='pt')
-        bot_input_ids = torch.cat([chat_history_ids[:,-10:], new_user_input_ids], dim=-1) if chat_history_ids.shape[0] > 0 else new_user_input_ids
-        chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
-        response = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
-        print("- DialoGPT: {}".format(response))
-
-        # Export the result 
-        with open(path + 'response.txt', mode ='w') as file:
-            file.write(response)
         
+        if (("ok" in [word.lower() for word in text.split()]) & (len(text.split()) <= 3)): # Reset context
+            chat_history_ids = torch.tensor([])
+            bot_input_ids = torch.tensor([])
+            print("- DialoGPT: .")
+            continue
+        
+        elif wiki_search: # Answer from wikipedia
+            wiki_search = False
+            search = wikipedia.search(text, results=1)
+            if len(search) == 0:
+                response = "I can't find anything about" + text + "on wikipedia."
+            else:
+                response = wikipedia.summary(search[0], auto_suggest = False, sentences = 2)
+                
+        elif ("wikipedia" in [word.lower() for word in text.split()]): # Wikipedia request
+            response = "What do you want me to search on wikipedia?"
+            wiki_search = True
+            
+        else : # Generate an answer with Dialogue GPT
+            new_user_input_ids = tokenizer.encode(text + tokenizer.eos_token, return_tensors='pt')
+            bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if chat_history_ids.shape[0] > 0 else new_user_input_ids
+            chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+            response = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+            
+        
+        print("- DialoGPT: {}".format(response))
+        # Export the result 
+        with open(path + 'response.txt', mode ='w', encoding="utf-8") as file:
+            file.write(response)
+    
+    else:
         # Wait a little bit
         time.sleep(0.3)
